83232c3a239b86ec3ef232f6ce3767c7dc57f373
bhargav-13 <bhargavthesiya11@gmail.com>
2023-06-27 22:47:36 +0530

    27-06

--- DjangoPractice/polls/urls.py
+++ DjangoPractice/polls/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('room/<str:pk>', views.room, name="room")
+    path('room/<str:pk>', views.room, name="room"),
+    path('createroom/', views.Createroom, name="create-room"),
+    path('updateroom/<str:pk>', views.updateRoom, name="update-room"),
+    path('deleteroom/<str:pk>', views.DeleteRoom, name="delete-room"),
 ]
--- DjangoPractice/polls/views.py
+++ DjangoPractice/polls/views.py
@@ -1,12 +1,58 @@
-from django.shortcuts import render
-from .models import Room
+from django.shortcuts import render, redirect
+from django.db.models import Q
+from .models import Room, Topic
+from .forms import RoomForm
 
 def home(request):
-    rooms = Room.objects.all()
-    context = { 'rooms' : rooms }
+    if request.GET.get('q') != None:
+        q = request.GET.get('q')
+    else:
+        q = ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains = q) |
+        Q(name__icontains = q) |
+        Q(description__icontains = q)
+    )
+
+    topic = Topic.objects.all()
+    room_count = rooms.count()
+    context = { 'rooms' : rooms, 'topics' : topic, 'room_count' : room_count }
     return render(request, 'polls/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    context = {'room': room,}
     return render(request, 'polls/room.html', context)
+
+def Createroom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form' : form}
+    return render(request, 'polls/room_forn.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form' : form}
+    return render(request, 'polls/room_forn.html', context)
+
+def DeleteRoom(request, pk):
+    room = Room.objects.get(id = pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    return render(request, 'polls/delete.html', {'obj' : room})
