4161125941404a057773ae974ea14a7c07a0c222
bhargav-13 <bhargavthesiya11@gmail.com>
2023-06-29 23:14:45 +0530

    29-06

--- DjangoPractice/polls/urls.py
+++ DjangoPractice/polls/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.LoginPage, name="login" ),
+    path('logout/', views.logoutUser, name="logout" ),
+    path('register/', views.RegisterPage, name="register" ),
+
     path('', views.home, name="home"),
     path('room/<str:pk>', views.room, name="room"),
+
     path('createroom/', views.Createroom, name="create-room"),
     path('updateroom/<str:pk>', views.updateRoom, name="update-room"),
     path('deleteroom/<str:pk>', views.DeleteRoom, name="delete-room"),
+    path('delete-msg/<str:pk>', views.DeleteMsg, name="delete-msg"),
+
 ]
--- DjangoPractice/polls/views.py
+++ DjangoPractice/polls/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login, logout
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     if request.GET.get('q') != None:
@@ -15,6 +21,7 @@ def home(request):
         Q(description__icontains = q)
     )
 
+
     topic = Topic.objects.all()
     room_count = rooms.count()
     context = { 'rooms' : rooms, 'topics' : topic, 'room_count' : room_count }
@@ -22,9 +29,23 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room,}
+    messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if request.method == 'POST':
+        msg = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk = room.id)
+
+
+
+    context = {'room': room, 'room_messages' : messages, 'participants':  participants}
     return render(request, 'polls/room.html', context)
 
+@login_required(login_url= '/login')
 def Createroom(request):
     form = RoomForm()
     if request.method == 'POST':
@@ -36,9 +57,13 @@ def Createroom(request):
     context = {'form' : form}
     return render(request, 'polls/room_forn.html', context)
 
+@login_required(login_url= '/login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse("<h1>You are not Allowed Here!!</h1>")
 
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room)
@@ -49,10 +74,74 @@ def updateRoom(request, pk):
     context = {'form' : form}
     return render(request, 'polls/room_forn.html', context)
 
+@login_required(login_url= '/login')
 def DeleteRoom(request, pk):
     room = Room.objects.get(id = pk)
+
+    if request.user != room.host:
+        return HttpResponse("<h1>Only user can delete this!!</h1>")
+
     if request.method == 'POST':
         room.delete()
         return redirect('home')
 
     return render(request, 'polls/delete.html', {'obj' : room})
+
+def LoginPage(request):
+    page = 'login'
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username= username)
+        except:
+            messages.error(request, "User Doesn't Exist")
+
+        user = authenticate(request, username=username, password= password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password is invalid')
+
+    context = {'page' : page}
+    return render(request, 'polls/login_register.html', context)
+
+def logoutUser(request):
+
+    logout(request)
+
+    return redirect('home')
+
+def RegisterPage(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "An Error During Registration")
+
+    return render(request, 'polls/login_register.html', {'form' : form})
+
+@login_required(login_url= '/login')
+def DeleteMsg(request, pk):
+    message = Message.objects.get(id = pk)
+
+    if request.user != message.user:
+        return HttpResponse("<h1>Only user can delete this!!</h1>")
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+
+    return render(request, 'polls/delete.html', {'obj' : message})
+
